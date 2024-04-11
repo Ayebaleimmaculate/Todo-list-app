2d0765ac4da2e472a3cbee871b90213e2915bae5
Ayebaleimmaculate <142968605+Ayebaleimmaculate@users.noreply.github.com>
2024-04-11 22:00:16 +0300

    git init

---
+++ Todo app/todo.js
@@ -0,0 +1,38 @@
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+function addTask(){
+    if (todoInput.value === ''){
+        alert('you must add something');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = todoInput.value;
+        todoList.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    todoInput.value = '';
+    saveData();
+}
+todoList.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem('data',todoList.innerHTML);
+
+}
+function showTask(){
+    todoList.innerHTML = localStorage.getItem('data');
+}
+showTask();
+
